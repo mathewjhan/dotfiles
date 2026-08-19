d9149d93792561017aaa9f79a40f179b99d635c3
Mathew Han <mathewjhan@gmail.com>
2026-08-18 20:30:07 -0700

    use octo

--- nvim/.config/nvim/lua/plugins/git.lua
+++ nvim/.config/nvim/lua/plugins/git.lua
@@ -52,4 +52,34 @@ return {
       require("differ").setup()
     end,
   },
+
+  -- GitHub issues/PRs: browse, create, review, diff
+  {
+    "pwntester/octo.nvim",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "ibhagwan/fzf-lua",
+      "nvim-tree/nvim-web-devicons",
+    },
+    config = function()
+      require("octo").setup({
+        picker = "fzf-lua",
+        use_local_fs = false, -- use local files on right side of reviews
+        reviews = {
+          auto_show_threads = true,
+          focus = "right", -- focus right buffer on diff open
+        },
+      })
+
+      -- Search my open PRs in the repo of the current buffer
+      vim.keymap.set("n", "<leader>gp", function()
+        local repo = require("octo.utils").get_remote_name()
+        if not repo or repo == "" then
+          vim.notify("octo: no git remote found", vim.log.levels.WARN)
+          return
+        end
+        vim.cmd("Octo search is:pr is:open author:@me repo:" .. repo)
+      end, { desc = "My open PRs (this repo)" })
+    end,
+  },
 }
